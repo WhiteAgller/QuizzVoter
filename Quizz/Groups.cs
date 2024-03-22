94209b0dfc7f8817289aff72f4fcf8963c17882d
Jan Straka <514615@mail.muni.cz>
2024-03-22 01:12:35 +0100

    added rest

---
+++ Quizz/Groups.cs
@@ -0,0 +1,12 @@
+﻿namespace Quizz;
+
+public class Group
+{
+    public char Key { get; set; }
+    public string[] Participants { get; set; }
+}
+
+public class GroupList
+{
+    public List<Group> Groups { get; set; }
+}
---
+++ Quizz/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Quizz;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false, true)
+    .Build();
+
+var groups = config.Get<GroupList>();
+
+//For padding
+var maxCharName = groups!.Groups.Max(x => String.Join("", x.Participants).Length);
+var numberOfMostParticipants = groups!.Groups.MaxBy(x => String.Join("", x.Participants).Length)!.Participants.Length;
+var padding = maxCharName + numberOfMostParticipants;
+
+var alreadyPressed = new List<char>();
+var isFirst = true;
+
+Stopwatch sw = new Stopwatch();
+
+Console.WriteLine("Start");
+do {
+    while (! Console.KeyAvailable)
+    {
+        var key = Console.ReadKey(true);
+
+        foreach (var group in groups!.Groups)
+        {
+            if (key.KeyChar == group.Key && !alreadyPressed.Contains(key.KeyChar))
+            {
+                if (isFirst)
+                {
+                    sw.Start();
+                    isFirst = false;
+                }
+
+                var ts = sw.Elapsed;
+                alreadyPressed.Add(key.KeyChar);
+                Console.WriteLine(String.Join("/", group.Participants).PadRight(padding) + "| " + String.Format("{0:00}:{1:00}", ts.Seconds, ts.Milliseconds));
+            }
+        }
+        
+        if (key.KeyChar == 'r')
+        {
+            sw.Stop();
+            sw.Reset();
+            alreadyPressed.Clear();
+            isFirst = true;
+            Console.Clear();
+            Console.WriteLine("Next round:");
+        }
+
+    }       
+} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
